9b5fa4c4b8195cbe2a5858d5621c98580a349454
David Fraser <david.fraser@hexagon.com>
2020-10-08 14:36:19 +0200

    Fix obfuscated layout code to work with Python 3. Adjusted to use new URL

--- layout_code.py
+++ layout_code.py
@@ -1,6 +1,6 @@
 import sys
-r=raw_input
-l=list(open('calcpi-compressed.html', 'rb').read())
+r=input
+l=list(open('calcpi-compressed.html', 'r').read())
 w=sys.stdout.write
-for c in"\n".join([r()for _ in[1]*input()]):w(c=='*'and(l and l.pop(0)or'.')or c)
+for c in"\n".join([r()for _ in[1]*int(r())]):w(c=='*'and(l and l.pop(0)or'.')or c)
 w("".join(l))
